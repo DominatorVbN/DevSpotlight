75f9d0f347b254d0c2e013eba7d49ec85c8ec82f
Amit Samant <amit.samant@gojek.com>
2023-10-15 22:38:28 +0530

    Impemented Apple Signin

---
+++ DevSpotlight/AuthView/AuthView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthView.swift
+//  DevSpotlight
+//
+//  Created by Amit Samant on 15/10/23.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthView()
+}
---
+++ DevSpotlight/KeychainHelper.swift
@@ -0,0 +1,8 @@
+//
+//  KeychainHelper.swift
+//  DevSpotlight
+//
+//  Created by Amit Samant on 15/10/23.
+//
+
+import Foundation
